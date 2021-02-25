9c13b51ded2cd661f4c1e88d15b2cbdffc3c27eb
Ammeister <hippolyte.souvay@yahoo.fr>
2021-02-25 14:19:46 +0100

    Création d'une fonction 'ImportTEI' dans le module 'ModuleImport.py'.

--- ScriptsPython/ModulesPython/ModuleImport.py
+++ ScriptsPython/ModulesPython/ModuleImport.py
@@ -3,10 +3,13 @@
 from pathlib import Path
 
 def ImportTXT(NomFichier):
-    Repertoire = Path("../Sources/EditionsTXT/")
-    DocumentTXT = Repertoire / NomFichier
+    RepertoireTXT = Path("../Sources/EditionsTXT/")
+    DocumentTXT = RepertoireTXT / NomFichier
     SourceTXT = open(DocumentTXT)
     print(SourceTXT.read())
 
-def foo():
-    print ("foo!")
+def ImportTEI(NomFichier):
+    RepertoireTEI = Path("../Sources/EditionsXML/")
+    DocumentTEI = RepertoireTEI / NomFichier
+    SourceTEI = open(DocumentTEI)
+    print(SourceTEI.read())
--- ScriptsPython/ModulesPython/__init__.py
+++ ScriptsPython/ModulesPython/__init__.py
@@ -1,3 +1,5 @@
 #coding:utf-8
 
 from .ModuleImport import ImportTXT
+
+from .ModuleImport import ImportTEI
